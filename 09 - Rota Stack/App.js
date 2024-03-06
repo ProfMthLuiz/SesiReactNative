ddac9095d3e1d9cabfb31e6ede313d07bc8f75de
ProfMatheus <154000567+ProfMthLuiz@users.noreply.github.com>
2024-03-06 18:48:01 -0300

    Update

--- 09 - Rota Stack/App.js
+++ 09 - Rota Stack/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 // Stack Navigation
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native"; // Importa o componente NavigationContainer de "@react-navigation/native" para envolver a navegação da aplicação.
+import { createNativeStackNavigator } from "@react-navigation/native-stack"; // Importa a função createNativeStackNavigator de "@react-navigation/native-stack" para criar um navegador de pilha nativo.
 
-// Paginas
-import Home from "./src/pages/Home";
-import Sobre from "./src/pages/Sobre";
+// Páginas
+import Home from "./src/pages/Home"; // Importa o componente Home da pasta "./src/pages/Home".
+import Sobre from "./src/pages/Sobre"; // Importa o componente Sobre da pasta "./src/pages/Sobre".
 
 // Cria uma instância de um navegador de pilha usando createNativeStackNavigator e a armazena na constante Stack.
 // Isso permite que você use esse navegador de pilha para definir a navegação em sua aplicação.
@@ -14,30 +14,11 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Sobre" component={Sobre} />
+    <NavigationContainer> {/* Componente fornecido pelo React Navigation para envolver a navegação da aplicação. */}
+      <Stack.Navigator initialRouteName="Home"> {/* Componente Navigator do navegador de pilha. initialRouteName define a rota inicial da navegação. */}
+        <Stack.Screen name="Home" component={Home} /> {/* Define uma tela com o nome "Home" e associa o componente Home a ela. */}
+        <Stack.Screen name="Sobre" component={Sobre} /> {/* Define uma tela com o nome "Sobre" e associa o componente Sobre a ela. */}
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
