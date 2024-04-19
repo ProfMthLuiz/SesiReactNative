df72f3e7ec56ccfd150a777f0696ea1f51c6276d
ProfMatheus <154000567+ProfMthLuiz@users.noreply.github.com>
2024-04-19 09:48:05 -0300

    Update App.js

--- 16 - FlatList/App.js
+++ 16 - FlatList/App.js
@@ -1,20 +1,24 @@
 import { View, Text, FlatList } from "react-native";
 import { styles } from "./src/styles/Style";
+import TxtComponent from "./src/componentes/TxtComponent";
 
 export default function App() {
-  const data = [
-    { id: "1", backgroundColor: "red" },
-    { id: "2", backgroundColor: "pink" },
-    { id: "3", backgroundColor: "blue" },
-    { id: "4", backgroundColor: "yellow" },
-    { id: "5", backgroundColor: "green" },
+  const dados = [
+    { id: "1", backgroundColor: "red", text: "TEXTO" },
+    { id: "2", backgroundColor: "pink", text: "TEXTO" },
+    { id: "3", backgroundColor: "blue", text: "TEXTO" },
+    { id: "4", backgroundColor: "yellow", text: "TEXTO" },
+    { id: "5", backgroundColor: "green", text: "TEXTO" },
   ];
 
-  // Renderiza os Componentes Views(box) com seu respectivo Texto.
-  // Recebe nosso array de objeto, nomeado como data!
-  const renderItem = ({ item }) => (
+  // Função que renderiza os componentes Views (caixas) com seus respectivos Textos.
+  // Recebe o objeto atual como argumento { item }.
+  // O argumento está entre chaves {} devido à desestruturação, permitindo o acesso direto às propriedades do objeto.
+  const rdItem = ({ item }) => (
+    // Renderiza uma View com estilo combinado com o estilo da caixa (box) e o backgroundColor do item atual.
     <View style={[styles.box, { backgroundColor: item.backgroundColor }]}>
-      <Text style={styles.txt}>React Native!</Text>
+      {/* Renderiza um Texto com estilo definido por styles.txt e o texto do item atual. */}
+      <Text style={styles.txt}>{item.text}</Text>
     </View>
   );
 
@@ -22,9 +26,9 @@ export default function App() {
     <View style={styles.container}>
       <View style={styles.header}></View>
       <FlatList
-        data={data} // Ele está recebendo todos os objetos do nosso array data
-        renderItem={renderItem} // Chama a função para renderizar as Views box
-        keyExtractor={(data) => data.id} // Extrair o id do objeto
+        data={dados} // Ele está recebendo todos os objetos do nosso array data
+        renderItem={rdItem} // Chama a função para renderizar as Views box
+        keyExtractor={(dados) => dados.id} // Extrair o id do objeto
       />
       <View style={styles.footer}></View>
     </View>
