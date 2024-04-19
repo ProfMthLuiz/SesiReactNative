8351b382ce6efaf5cc0fa6045f35dc3fe4f838d0
ProfMatheus <154000567+ProfMthLuiz@users.noreply.github.com>
2024-04-19 09:49:56 -0300

    Update App1.js

--- 16 - FlatList/App1.js
+++ 16 - FlatList/App1.js
@@ -2,22 +2,31 @@ import { View, Text, FlatList } from "react-native";
 import { styles } from "./src/styles/Style";
 import TxtComponent from "./src/componentes/TxtComponent";
 
+// Função principal do aplicativo.
 export default function App() {
+  // Definindo os dados a serem exibidos na FlatList.
   const dados = [{ id: "1", component: <TxtComponent /> }];
 
+  // Função para renderizar cada item da FlatList.
   const rdItem = ({ item }) => {
+    // Retorna o componente específico do item.
     return item.component;
   };
 
+  // Renderização do componente App.
   return (
     <View style={styles.container}>
+      {/* Componente View para o cabeçalho. */}
       <View style={styles.header}></View>
+      
       <FlatList
         horizontal={true}
-        data={dados} // Ele está recebendo todos os objetos do nosso array data
-        renderItem={rdItem} // Chama a função para renderizar as Views box
-        keyExtractor={(dados) => dados.id} // Extrair o id do objeto
+        data={dados} // Recebe os dados a serem exibidos na lista.
+        renderItem={rdItem} // Função para renderizar cada item da lista.
+        keyExtractor={(dados) => dados.id} // Extrai a chave única de identificação de cada item.
       />
+      
+      {/* Componente View para o rodapé. */}
       <View style={styles.footer}></View>
     </View>
   );
