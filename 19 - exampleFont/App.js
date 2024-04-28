4e217796179e3431a7028cb6a7d14995312cfd2d
ProfMthLuiz <mthluiz99@gmail.com>
2024-04-28 09:41:30 -0300

    exemplo de como usar fontes no EXPO GO

---
+++ 19 - exampleFont/App.js
@@ -0,0 +1,54 @@
+import { StyleSheet, Text, View } from "react-native";
+
+import { useFonts } from "expo-font";
+
+import { useState } from "react";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    "Roboto-Light": require("./assets/fonts/Roboto-Light.ttf"),
+    "Jacquard24-Regular": require("./assets/fonts/Jacquard24-Regular.ttf"),
+  });
+
+  const [notLoadedFont, setNotLoadedFont] = useState(false);
+
+  if (!fontsLoaded) {
+    setNotLoadedFont(true);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text
+        style={{ fontFamily: notLoadedFont ? "" : "Roboto-Bold", fontSize: 30 }}
+      >
+        Roboto Bold
+      </Text>
+      <Text
+        style={{
+          fontFamily: notLoadedFont ? "" : "Roboto-Light",
+          fontSize: 30,
+        }}
+      >
+        Roboto-Light
+      </Text>
+      <Text
+        style={{
+          fontFamily: notLoadedFont ? "" : "Jacquard24-Regular",
+          fontSize: 30,
+        }}
+      >
+        Jacquard24-Regular
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
