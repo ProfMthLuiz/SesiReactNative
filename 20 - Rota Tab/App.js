298344a656a8b79a7c19b44872068d674c146b76
ProfMatheus <154000567+ProfMthLuiz@users.noreply.github.com>
2024-05-07 16:51:10 -0300

    Update App.js

--- 20 - Rota Tab/App.js
+++ 20 - Rota Tab/App.js
@@ -1,40 +1,85 @@
 import { NavigationContainer } from "@react-navigation/native";
-
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import HomeScreen from "./src/pages/Home";
-import SettingsScreen from "./src/pages/SettingsScreen";
-
+import Home from "./src/pages/Home";
+import Settings from "./src/pages/SettingsScreen";
+import Sobre from "./src/pages/Sobre";
+import Contato from "./src/pages/Contato";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-import Ionicons from "@expo/vector-icons/Ionicons";
-import { AntDesign } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      {/* Componente fornecido pelo React Navigation para envolver a navegação da aplicação. */}
       <Tab.Navigator
         screenOptions={{
-          tabBarActiveTintColor: "#FFF",
-          tabBarStyle: { backgroundColor: "#000" },
+          tabBarActiveTintColor: "purple",
+          tabBarInactiveTintColor: "black",
+          tabBarStyle: {
+            backgroundColor: "#fff",
+            height: 50,
+            position: "absolute",
+            bottom: 20,
+            right: 20,
+            left: 20,
+            borderTopEndRadius: 15,
+            borderTopColor: "transparent",
+            padding: 10,
+            shadowColor: "purple",
+            shadowOffset: {
+              width: 0,
+              height: 4,
+            },
+            shadowOpacity: 0.30,
+            shadowRadius: 4.65,
+            
+            elevation: 8,
+          },
         }}
       >
-        {/* Componente Navigator do navegador tab.*/}
         <Tab.Screen
-          options={{ headerShown: false }}
           name="Home"
-          component={HomeScreen}
+          component={Home}
+          options={{
+            tabBarIcon: ({ size, color }) => {
+              return <FontAwesome size={size} color={color} name="home" />;
+            },
+            headerShown: false,
+            title: '',
+          }}
         />
         <Tab.Screen
+          name="Screen"
+          component={Settings}
           options={{
+            tabBarIcon: ({ size, color }) => {
+              return <FontAwesome size={size} color={color} name="search" />;
+            },
             headerShown: false,
-            tabBarIcon: ({ color, size }) => {
+            title: ''
+          }}
+        />
+        <Tab.Screen
+          name="Sobre"
+          component={Sobre}
+          options={{
+            tabBarIcon: ({ size, color }) => {
+              return <FontAwesome size={size} color={color} name="newspaper-o" />;
+            },
+            headerShown: false,
+            title: ''
+          }}
+        />
+        <Tab.Screen
+          name="Contato"
+          component={Contato}
+          options={{
+            tabBarIcon: ({ size, color }) => {
               return <FontAwesome size={size} color={color} name="envelope" />;
             },
+            headerShown: false,
+            title: ''
           }}
-          name="Screen"
-          component={SettingsScreen}
         />
       </Tab.Navigator>
     </NavigationContainer>
